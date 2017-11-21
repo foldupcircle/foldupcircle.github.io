1d314851730070dbfbee39207051b0dc0e41ce35
Bob <sriguddanti@gmail.com>
2017-11-21 01:05:07 +0000

    index.html

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-function openCity(cityName, elmnt, color) {
-    var i, tabcontent, focus;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+function openCity(evt, cityName) {
+    var i, tablinks;
 
-    focus = document.getElementsByClassName("tablink");
-    for (i = 0; i < focus.length; i++) {
-        focus[i].style.backgroundColor = "";
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i<tablinks.length; i++) {
+        tablinks[i].style.display = "none";
     }
-
     document.getElementById(cityName).style.display = "block";
-
-    elmnt.style.backgroundColor = color;
 }
-
-document.getElementById("defaultOpen").click();
